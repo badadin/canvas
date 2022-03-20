8046d53d6c4ee3263c56e890659ae326989f1cd2
badadin <badadinf@mail.ru>
2022-03-20 11:05:48 +0400

    package management, t2 starting

---
+++ app/src/main/java/com/rinatvasilev/canvas/ui/views/Banana.kt
@@ -0,0 +1,74 @@
+package com.rinatvasilev.canvas.t1avocado
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rinatvasilev.canvas.ui.theme.CanvasTheme
+
+@Composable
+fun Banana() {
+
+    Canvas(modifier = Modifier.size(250.dp)) {
+
+        val width = size.width
+        val height = size.height
+
+        val pathContour = Path().apply {
+            moveTo(width.times(.05f), height.times(.5f))
+
+            cubicTo(
+                x1 = width.times(.10f),
+                y1 = height.times(.9f),
+                x2 = width.times(1.15f),
+                y2 = height.times(.9f),
+                x3 = width.times(.9f),
+                y3 = height.times(.5f)
+            )
+
+            quadraticBezierTo(
+                x1 = width.times(.88f),
+                y1 = height.times(.47f),
+                x2 = width.times(.83f),
+                y2 = height.times(.52f)
+            )
+
+            cubicTo(
+                x1 = width.times(.8f),
+                y1 = height.times(.7f),
+                x2 = width.times(.3f),
+                y2 = height.times(.65f),
+                x3 = width.times(.1f),
+                y3 = height.times(.47f)
+            )
+
+            cubicTo(
+                x1 = width.times(.7f),
+                y1 = height.times(.7f),
+                x2 = width.times(.8f),
+                y2 = height.times(.5f),
+                x3 = width.times(.83f),
+                y3 = height.times(.52f)
+            )
+        }
+
+        drawPath(
+            path = pathContour,
+            color = Color(0xFF442B1A),
+            style = Stroke(width = 5f)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BananaPreview() {
+    CanvasTheme {
+        Banana()
+    }
+}
